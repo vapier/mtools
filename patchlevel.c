2fd3c43356caa1610d40f1a47c6b262ccb630f4d
Alain Knaff <alain@knaff.lu>
2006-02-28 09:00:13 +0000

    If neither dir not dir.info Info directory exists, use dir
    git-svn-id: svn://svn.savannah.gnu.org/mtools/trunk@111 8fbfe7c3-7cdf-472d-ae67-d119146b2a64

--- patchlevel.c
+++ patchlevel.c
@@ -1,2 +1,5 @@
-const char *mversion="3.9.10b";
-const char *mdate = "February 27th, 2006";
+const char *mversion="3.9.10";
+
+/* Multiple releases on same day should be marked with (b), (cd), (d) after
+ * date string below */
+const char *mdate = "February 28th, 2006";
