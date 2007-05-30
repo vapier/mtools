1ff5f95f2d6f1e0b6afded18d5901662eeb21862
Alain Knaff <alain@knaff.lu>
2007-05-30 11:23:27 +0000

    New version written in Bourne Shell rather than C shell
    git-svn-id: svn://svn.savannah.gnu.org/mtools/trunk@180 8fbfe7c3-7cdf-472d-ae67-d119146b2a64

--- scripts/amuFormat.sh
+++ scripts/amuFormat.sh
@@ -1,164 +1,87 @@
-#!/bin/csh 
+#!/bin/sh
 #
 #  amuFormat.sh  Formats various types and sizes of PC-Cards, according to the
 #  AMU-specification
-#  
+#
 #  parameters:   $1:   Card Type: The Card Type is written as disk/volume-label
 #                      to the boot-record
 #                      The string should have a length of max. 11 characters.
-# 
+#
 #                $2:   Drive character (b:, c:)
 #
 #  10-12-2003    lct   created
 #
-set vers=1.4
-set fdrive="f:"
-
+vers=1.4
 
 #echo "debug: $0,$1,$2,$3,$4"
 
 #
 # main()
 #
+if [ $# -ne 2 ] ; then
+	echo "Usage: amuFormat.sh <Card Type> <drive>" >&2
+	echo "<Card Type> has to be defined in amuFormat.sh itself" >&2
+	echo "<drive> has to be defined in mtools.conf" >&2
+	exit 1
+fi
+
 echo "amuFormat $vers started..."
 
-if ( $#argv == 0 ) then
-   echo "Usage: amuFormat.sh <Card Type> <drive>"
-   echo "<Card Type> has to be defined in amuFormat.sh itself"
-   echo "<drive> has to be defined in mtools.conf"
-   echo ""
-   exit 0
-endif
+drive="$2"
 
+case "$1" in
+8MBCARD-FW)
+	## using the f: or g: drive for fat12 formatting...
+	## see mtools.conf file...
+	case "$2" in
+	[bB]:) drive="f:" ;;
+	[cC]:) drive="g:" ;;
+	*) echo "Drive $2 not supported."; exit 1 ;;
+	esac
+	cylinders=245 heads=2 cluster_size=8
+	;;
+32MBCARD-FW)
+	#from amu_toolkit_0_6:
+	#mformat -t489 -h4 -c4 -n32 -H32 -r32 -vPC-CARD -M512 -N0000 c:
+	cylinders=489 heads=4 cluster_size=4
+	;;
+64MBCARD-FW)
+	echo "***** WARNING: untested on AvHMU, exiting *****"
+	exit 1
+	cylinders=245 heads=2 cluster_size=8
+	;;
+1GBCARD-FW)
+	# from amu_toolkit_0_6:
+	#mformat -t2327 -h16 -c64 -n63 -H63 -r32 -v AMU-CARD -M512 -N 0000 c:
+	echo "***** WARNING: untested on AvHMU *****"
+	cylinders=2327 heads=16 cluster_size=64
+	;;
+64MBCARDSAN)
+	# from amu_toolkit_0_6:
+	#mformat -t489 -h8 -c4 -n32 -H32 -r32 -v AMU-CARD -M512 -N 0000 c:
+	cylinders=489 heads=8 cluster_size=4
+	;;
+#
+# insert new cards here...
+#
+*)
+	echo "Card not supported."
+	exit 1
+	;;
+esac
 
 echo "Formatting card in slot $2 as $1"
 
-if ( $1 == "8MBCARD-FW" ) then
-   
-   ## determine formatting drive
-   if ( ($2 == "b:") || ($2 == "B:") ) then
-      set fdrive = "f:"
-   else if ( ($2 == "c:") || ($2 == "C:" ) ) then
-           set fdrive = "g:"
-   else
-      echo "Drive $2 not supported."
-      exit 1
-   endif
-         
-   ## initialise partition table
-   mpartition -I $fdrive
-   
-   # write a partition table
-   mpartition -c -t245 -h2 -s32 -b32 $fdrive
+## initialise partition table
+mpartition -I "$drive"
 
-   ## using the f: or g: drive for fat12 formatting...
-   ## see mtools.conf file...
-   mformat -c8 -v 8MBCARD-FW $fdrive
- 
-   minfo $2
-   mdir  $2
-   
-   echo "done."
-   exit 0
+# write a partition table
+mpartition -c -t$cylinders -h$heads -s32 -b32 "$drive"
 
-endif
+## write boot-record, two FATs and a root-directory
+mformat -c$cluster_size -v "$1" "$drive"
 
-if ( $1 == "32MBCARD-FW" ) then
-   
-   #from amu_toolkit_0_6: mformat -t489 -h4 -c4 -n32 -H32 -r32 -vPC-CARD -M512 -N0000 c:
-  
-   ## initialise partition table
-   mpartition -I $2
+minfo "$2"
+mdir  "$2"
 
-   ## write a partition table
-   mpartition -c -t489 -h4 -s32 -b32 $2
-
-   ## write boot-record, two FATs and a root-directory 
-   mformat -c4 -v 32MBCARD-FW $2
-
-   minfo $2
-   mdir  $2
-
-   echo "done."
-   exit 0
-
-endif
-
-if ( $1 == "64MBCARD-FW" ) then
-
-   echo "***** WARNING: untested on AvHMU, exiting *****"
-   exit 0
-
-   ## initialise partition table
-   mpartition -I $2
-
-   ## write a partition table
-   mpartition -c -t245 -h2 -s32 -b32 $2
-
-   ## write boot-record, two FATs and a root-directory
-   mformat -c8 -v 64MBCARD-FW $2
-
-   minfo $2
-   mdir  $2
-
-   echo "done."
-   exit 0
-
-
-endif
-
-
-if ( $1 == "1GBCARD-FW" ) then
-
-   # from amu_toolkit_0_6: mformat -t2327 -h16 -c64 -n63 -H63 -r32 -v AMU-CARD -M512 -N 0000 c:
-   
-   echo "***** WARNING: untested on AvHMU *****"
-   
-   ## initialise partition table
-   mpartition -I $2
-
-   # write a partition table
-   mpartition -c -t2327 -h16 -s32 -b32 $2
-
-   ## write boot-record, two FATs and a root-directory
-   mformat -c64 -v 1GBCARD-FW $2
-
-   minfo $2
-   mdir  $2
-
-   echo "done."
-   exit 0
-
-
-endif
-
-if ( $1 == "64MBCARDSAN" ) then
-   
-   # from amu_toolkit_0_6: mformat -t489 -h8 -c4 -n32 -H32 -r32 -v AMU-CARD -M512 -N 0000 c:
-
-   ## initialise partition table
-   mpartition -I $2
-
-   # write a partition table
-   mpartition -c -t489 -h8 -s32 -b32 $2
-
-   ## write boot-record, two FATs and a root-directory
-   mformat -c4 -v 64MBCARDSAN $2
-
-   minfo $2
-   mdir  $2
-
-   echo "done."
-   exit 0
-
-
-endif
-
-#
-# insert new cards here...
-# 
-
-echo "Card not supported."
-exit 1
- 
- 
+echo "done."
