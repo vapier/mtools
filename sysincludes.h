a6c0aa85b404141f94ef151238f32095f8c5a06f
Alain Knaff <alain@knaff.lu>
2009-02-09 20:22:28 +0000

    Added GNU copyright everywhere
    git-svn-id: svn://svn.savannah.gnu.org/mtools/trunk@242 8fbfe7c3-7cdf-472d-ae67-d119146b2a64

--- sysincludes.h
+++ sysincludes.h
@@ -1,4 +1,21 @@
-/* System includes for mtools */
+/*
+ *  This file is part of mtools.
+ *
+ *  Mtools is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Mtools is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Mtools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * System includes for mtools
+ */
 
 #ifndef SYSINCLUDES_H
 #define SYSINCLUDES_H
