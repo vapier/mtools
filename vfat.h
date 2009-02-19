233e89186cc8b6cb3cff3119d67e63e5c5e4a6ce
Alain Knaff <alain@knaff.lu>
2009-02-19 22:36:05 +0000

    ARM-compatible "PACKED" notice New copyright notice
    git-svn-id: svn://svn.savannah.gnu.org/mtools/trunk@268 8fbfe7c3-7cdf-472d-ae67-d119146b2a64

--- vfat.h
+++ vfat.h
@@ -1,7 +1,7 @@
 #ifndef MTOOLS_VFAT_H
 #define MTOOLS_VFAT_H
 
-/*
+/*  Copyright 2009 Alain Knaff.
  *  This file is part of mtools.
  *
  *  Mtools is free software: you can redistribute it and/or modify
@@ -28,7 +28,7 @@
 struct unicode_char {
 	unsigned char lchar;
 	unsigned char uchar;
-};
+} PACKED;
 
 
 /* #define MAX_VFAT_SUBENTRIES 32 */ /* Theoretical max # of VSEs */
