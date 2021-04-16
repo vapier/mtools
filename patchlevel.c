09b98d1dc3c448f513cd3cc665ee72a1b5165948
Alain Knaff <alain@knaff.lu>
2021-04-16 18:59:28 +0000

    Version 4.0.27
    git-svn-id: svn://svn.savannah.gnu.org/mtools/trunk@533 8fbfe7c3-7cdf-472d-ae67-d119146b2a64

--- patchlevel.c
+++ patchlevel.c
@@ -18,10 +18,10 @@
 #include "sysincludes.h"
 #include "msdos.h"
 
-const char *mversion="4.0.26";
+const char *mversion="4.0.27";
 
 /* Multiple releases on same day should be marked with (b), (cd), (d) after
  * date string below */
-const char *mdate = "November 28th, 2020";
+const char *mdate = "April 16th, 2021";
 
-const char *mformat_banner = "MTOO4026";
+const char *mformat_banner = "MTOO4027";
