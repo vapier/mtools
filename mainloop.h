593bc49c6892c99f4fef76841f9f225ec7e9da10
Alain Knaff <alain@knaff.lu>
2022-10-29 16:29:28 +0000

    - Specify in comments which applications use which field
    - removed unused fileName member
    
    
    git-svn-id: svn://svn.savannah.gnu.org/mtools/trunk@801 8fbfe7c3-7cdf-472d-ae67-d119146b2a64

--- mainloop.h
+++ mainloop.h
@@ -38,39 +38,57 @@ typedef struct MainParam_t {
 	void *arg; /* command-specific parameters
 		    * to be passed to callback */
 
-       	int openflags; /* flags used to open disk */
-	int lookupflags; /* flags used to lookup up using vfat_lookup */
+       	int openflags; /* flags used to open disk, supplied by application */
+	
+	int lookupflags; /* flags used to lookup up using vfat_lookup,
+			    supplied by application */
+	
 	int fast_quit; /* for commands manipulating multiple files, quit
-			* as soon as even _one_ file has a problem */
+			* as soon as even _one_ file has a
+			* problem. Supplied by application, used by
+			* mattrib and mcopy */
 
 	bounded_string shortname; /* where to put the short name of the
-				   * matched file */
+				   * matched file, used by mdir and mmove */
 	bounded_string longname; /* where to put the long name of the
-				  * matched file */
+				  * matched file, used by mdir and mmove */
 	/* out parameters */
-	Stream_t *File;
+	Stream_t *File; /* needed by mattrib */
 
-	direntry_t *direntry;  /* dir of this entry */
+	direntry_t *direntry;  /* dir of this entry: needed by mmove */
+
 	char *unixSourceName;  /* filename of the last opened Unix source
-				* file (Unix equiv of Dos direntry) */
+				* file (Unix equiv of Dos direntry). Only used
+				* when "copying" from Unix */
 
-	Stream_t *targetDir; /* directory where to place files */
-	char *unixTarget; /* directory on Unix where to put files */
+	Stream_t *targetDir; /* directory on Dos where to place files
+				or subdirectories. Needed by mcopy,
+				mmd, mmove */
 
+	const char *unixTarget; /* directory on Unix where to put files,
+				 * needed by mcopy */
+	
 	const char *targetName; /* basename of target file, or NULL if same
-				 * basename as source should be conserved */
+				 * basename as source should be conserved,
+				 * needed by mcopy, mmd, mmove */
+	
+	char *originalArg; /* original argument, complete with wildcards.
+			    * Used by mainloop internally for display in printed 
+			    * messages */
 
-	char *originalArg; /* original argument, complete with wildcards */
 	int basenameHasWildcard; /* true if there are wildcards in the
-				  * basename */
-
+				  * basename. Output only parameter,
+				  * needed by mdir, mcopy */
 
 	/* internal data */
-	char mcwd[MAX_PATH+4];
+	char mcwd[MAX_PATH+4]; /* needed by mcwd and mmove */
 
-	char *fileName; /* resolved Unix filename */
-
-	char targetBuffer[4*MAX_VNAMELEN+1]; /* buffer for target name */
+	char targetBuffer[4*MAX_VNAMELEN+1]; /* buffer for target
+						name, only used by
+						mpGetBasename() and
+						callers as a temporary
+						output for formatted
+						name */
 } MainParam_t;
 
 void init_mp(MainParam_t *MainParam);
